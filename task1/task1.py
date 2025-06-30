d54c9a369a1a56bd23ed1d962c22688f5e09161d
Aiken <chikitlee2001@gmail.com>
2025-06-30 18:54:05 +1000

    update comment for task 1

--- task1/task1.py
+++ task1/task1.py
@@ -1,8 +1,13 @@
+# Task: Given a list of digits, determine the integer that it represents. 
+# For example, given the list: [8,3,5,1], your program should output 8351 as an integer.
+
 def task(arr):
-    res = 0
-    n = len(arr)
+    res, n = 0, len(arr)
     for i in range(n):
         res += 10 ** (n - i - 1) * arr[i]
     print(res)
 
 task([8, 3, 5, 1])
+
+# 8351
+# Each digit has a place value
