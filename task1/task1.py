ca5162c25702f5874ef09e8cb221ff3e8ac1ce0f
Aiken <chikitlee2001@gmail.com>
2025-06-30 19:10:20 +1000

    update extra explaination for task1 and 2

--- task1/task1.py
+++ task1/task1.py
@@ -1,13 +1,16 @@
 # Task: Given a list of digits, determine the integer that it represents. 
 # For example, given the list: [8,3,5,1], your program should output 8351 as an integer.
 
+
+# Each digit has a place value, in the example of 8351. 8 is representing 1000*8, 3 is 100*3, 5 is 10*5, and 1 is 1*1
+# By multiply each digit by its place value and add them together, we can get the integer that we want
+
 def task(arr):
     res, n = 0, len(arr)
     for i in range(n):
+        # this formula is using the current array item(arr[i]) multiply by the place value
+        # 10 to the power of n-i-1, which is just how many item is there after this item.
         res += 10 ** (n - i - 1) * arr[i]
     print(res)
 
 task([8, 3, 5, 1])
-
-# 8351
-# Each digit has a place value
--- task2/task2.py
+++ task2/task2.py
@@ -1,16 +1,21 @@
 import random
 
+
 def generate_number():
+    # this is for generating a number between 1 and 100
     return random.randint(1, 100)
 
 def user_guess(low, high):
+    # The program will ask for a user input. The user is required to type in a input that is within the current range.
+    # And if the user didn't enter a valid input, the program will continue asking for input til the input is valid
     while True:
         guess = input(f"guess a number between {low} and {high}: ")
-        # checks if guess is a correct input
+        # checks if guess is a valid input
         if not guess.isdigit():
             print(f"please enter a number from {low} to {high}")
             continue
         guess = int(guess)
+        # checks if guess is in the range
         if guess < low or guess > high:
             print(f"please enter a number within {low} to {high}")
             continue
@@ -18,6 +23,7 @@ def user_guess(low, high):
             return guess
         
 def check_guess(ans, guess, low, high):
+    # check the user guess to see if it's correct, should be higher, or should be lower
     if guess > ans:
         print("Lower!")
         return low, guess
@@ -28,12 +34,15 @@ def check_guess(ans, guess, low, high):
         return ans, ans
 
 def game_over(tries):
+    # printing the winner message
     print(f"You win! You got it in {tries} tries!")
 
 def main():
+    # we first generate the random number as well as setting the range
     ans = generate_number()
     low, high = 1, 100
     tries = 0
+    # we keep looping through this until user gets the correct answer.
     while True:
         tries += 1
         guess = user_guess(low, high)
